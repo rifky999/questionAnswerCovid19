8ddc031fdda52b3df0588244305989e3cf6f9d9d
rifky999 <rifkyqw@gmail.com>
2020-04-05 22:20:42 +0700

    -

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 
+importScripts("https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js");
 
-const CACHE_NAME = "covid19-v6";
+const CACHE_NAME = "covid19-v7";
 
 var urlsToCache = [
     "/"
