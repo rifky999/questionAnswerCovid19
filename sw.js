612c6811e23c6d7812273c8296730cf24763b000
rifky999 <rifkyqw@gmail.com>
2020-04-05 21:06:28 +0700

    sw

---
+++ sw.js
@@ -0,0 +1,45 @@
+
+
+const CACHE_NAME = "covid19-v6";
+
+var urlsToCache = ["css/style.css"
+    , "css/bt.css"
+    , "js/bt.js"
+    , "js/jquery-3.3.1.min.js"
+    , "js/main.js"
+    , "js/jquery.steps.js"
+    , "js/ajx.js"
+];
+
+
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== CACHE_NAME && cacheName.startsWith("covid19")) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
